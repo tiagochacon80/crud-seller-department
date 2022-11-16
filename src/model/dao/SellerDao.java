8163c55d491fe6700b64dee6d0e20ffa90b7df29
Tiago Chacon <tiagochacon80@gmail.com>
2022-11-16 15:29:31 -0500

    SellerDaoJDBC and DaoFactory

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,5 +1,14 @@
 package model.dao;
 
-public interface SellerDao {
+import java.util.List;
 
+import model.entities.Seller;
+
+public interface SellerDao {
+	
+	void insert(Seller obj);
+	void update(Seller obj);
+	void deleteById(Seller obj);
+	Seller findById(Integer id);
+	List<Seller>  findAll();
 }
