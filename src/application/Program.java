0435e24e357dfd6e033c216e0656c8e6faf73334
Tiago Chacon <tiagochacon80@gmail.com>
2022-11-16 14:37:25 -0500

    Department entite class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,37 +1,12 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import db.DB;
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Connection conn = null;
-		Statement st = null;
-		ResultSet rs = null; 
-		try {
-			conn = DB.getConnection();
-			
-			st = conn.createStatement();
-			
-			rs = st.executeQuery("select * from department");
-			
-			while (rs.next()) {
-				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
-			}
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
-		finally {
-			DB.closeResulteSet(rs);
-			DB.closeStatement(st);
-			DB.closeConnection();
-		}
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
 	}
 }
